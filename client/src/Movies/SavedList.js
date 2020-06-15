a2bac5b60137b3119faa66fe6c45c32dc7c88dd6
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-15 18:15:19 -0500

    fixed bug on saved movies links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link} from 'react-router-dom'
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span key={movie.id} className="saved-movie">{movie.title}</span>
+      <Link key={movie.id} to={`/movies/${movie.id}`}>
+        <span  className="saved-movie">{movie.title}</span>
+      </Link>
     ))}
     <NavLink activeClassName="nav-link" to='/'>
     <div className="home-button">Home</div>
